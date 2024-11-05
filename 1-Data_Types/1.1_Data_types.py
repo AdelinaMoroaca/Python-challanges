01c46b3556d955a2250c72f580236579985bb253
AdelinaMoroaca <moroacaadelina@gmail.com>
2024-11-05 11:36:37 +0200

    Introduce of binary and complex

---
+++ 1-Data_Types/1.1_Data_types.py
@@ -0,0 +1,18 @@
+#Fundamentals Data Types
+int
+float
+
+bool
+str
+list
+tuple
+set 
+dict
+
+#Classes ->custom types
+Super Car
+
+#Specilized Data Types //special packages and modules that you can use from libraries
+Modules
+
+None
---
+++ 1-Data_Types/1.2_Fundamentals_Data_Types.py
@@ -0,0 +1,25 @@
+#Fundamentals Data types
+int
+float #needs more memory than int
+
+# #integers and float
+print(2 + 4);
+print(type(2+4));
+print(type(2-4));
+print(type(2*4));
+print(type(2/4));
+
+print(type(45.9));
+
+print(type(9.9+1.1));
+
+#3 pow2 = 9
+print(3**2);
+
+print(5 // 4)
+
+print(6 % 4)
+
+#math functions
+print(round(3.6));
+print(abs(-66));
